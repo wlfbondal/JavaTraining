b0c64c5c3b95291a92d2ee43d8365b771415e96d
wlfbondal <windy.l.bondal@gmail.com>
2025-12-02 18:05:02 +0800

    Update Activity1.java

--- BasicJavaActivity1/Activity1.java
+++ BasicJavaActivity1/Activity1.java
@@ -1,1 +1,26 @@
-
+/**
+*
+*/
+package com.bpi.helloworld.main;
+ 
+/**
+*
+*/
+ 
+import java.util.Scanner;
+ 
+public class Main {
+ 
+	public static void main(String[] args) {
+ 
+        Scanner scanner = new Scanner(System.in);
+ 
+        System.out.print("What is your name? ");
+        String name = scanner.nextLine(); //input
+        System.out.println("Hello, " + name + "!");
+ 
+        scanner.close();
+ 
+	}
+ 
+}
