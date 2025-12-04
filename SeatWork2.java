3ede58e73a3b7005dd6cf5ed29917f6f6c19da8e
Windy Lea F. Bondal <wlfbondal@headoffice.corp.bpi.com.ph>
2025-12-04 16:02:48 +0800

    Upload OOPActivity1 and other files

---
+++ SeatWork2.java
@@ -0,0 +1,11 @@
+package com.bpi.helloworld.main;
+
+public class SeatWork2 {
+    public static void main(String[] args) {
+
+        WhiteBoard wb = new WhiteBoard();
+        int result = wb.addNumbers(5, 3);
+        System.out.println("The total is: " + String.valueOf(result));
+    }
+}
+
---
+++ WhiteBoard.java
@@ -0,0 +1,9 @@
+package com.bpi.helloworld.main;
+
+public class WhiteBoard {
+
+    int addNumbers(int addend1, int addend2) {
+        int sum = addend1 + addend2;
+        return sum;
+    }
+}
