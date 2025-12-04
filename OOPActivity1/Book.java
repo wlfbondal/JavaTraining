e5299e8ea7544e41f0720c354d4bd9130e178e80
Windy Lea F. Bondal <wlfbondal@headoffice.corp.bpi.com.ph>
2025-12-04 15:13:21 +0800

    OOP1 Activity1

---
+++ OOPActivity1/Book.java
@@ -0,0 +1,12 @@
+package com.bpi.helloworld.main;
+
+
+public class Book {
+    // Attribute
+    String title = "Java OOP";
+
+    // Method
+    public void title() {
+        System.out.println("Book title: " + title);
+    }
+}
---
+++ OOPActivity1/House.java
@@ -0,0 +1,11 @@
+package com.bpi.helloworld.main;
+
+public class House {
+	
+	String houseAdd = "Quezon City";
+	
+    public void houseAdd() {
+        System.out.println("House Address: " + houseAdd);
+    }
+	
+}
